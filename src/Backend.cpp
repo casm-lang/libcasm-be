69ac22d4068b3895c17c01bca272e8cdc93c09b9
Philipp Paulweber <p.paulweber@gmail.com>
2016-08-29 15:10:12 +0200

    Repository
    * renamed origin repository URL for move from 'ppaulweber' to
      'casm-lang' namespace

--- src/Backend.cpp
+++ src/Backend.cpp
@@ -3,7 +3,7 @@
 //  All rights reserved.
 //  
 //  Developed by: Philipp Paulweber
-//                https://github.com/ppaulweber/libcasm-be
+//                https://github.com/casm-lang/libcasm-be
 //  
 //  This file is part of libcasm-be.
 //  
